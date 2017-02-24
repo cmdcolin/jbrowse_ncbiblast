9b2e25630875a6a27c3be12669343cda2a7f9352
Colin <colin.diesh@gmail.com>
2017-02-24 16:26:52 -0500

    Add ncbi button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,44 @@
 define([
     'dojo/_base/declare',
+    'dijit/MenuItem',
+    'dijit/registry',
     'JBrowse/Plugin'
 ],
 function(
     declare,
+    dijitMenuItem,
+    dijitRegistry,
     JBrowsePlugin
 ) {
     return declare(JBrowsePlugin, {
         constructor: function(/* args */) {
             console.log('NCBIBlast plugin starting');
-        }
+            this.initSearchMenu();
+        },
+
+        initSearchMenu: function()  {
+            var thisB = this;
+            this.browser.afterMilestone('initView', function() {
+                setTimeout(function() {
+                if (! thisB.init) {
+                    var blast = new dijitMenuItem({
+                        id: 'menubar_blast',
+                        label: 'NCBI BLAST+',
+                        onClick: function() {
+                            thisB.searchNCBI();
+                        }
+                    });
+                    thisB.browser.addGlobalMenuItem('tools', blast);
+                    thisB.browser.renderGlobalMenu('tools', { text: 'Tools' }, thisB.browser.menuBar );
+                    // move Tool menu in front of Help menu
+                    var toolsMenu = dijit.byId('dropdownbutton_tools');
+                    var helpMenu = dijit.byId('dropdownbutton_help');
+                    console.log(toolsMenu)
+                    dojo.place(toolsMenu.domNode, helpMenu.domNode, 'before');
+                    thisB.init = true;
+                }
+                }, 1000);
+            })
+        },
     });
 });
